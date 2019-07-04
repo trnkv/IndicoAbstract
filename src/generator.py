94861b1f124cd895aeb9727f67b56272ffb19634
trnkv <ttrnkv75@yandex.ru>
2019-07-04 14:34:41 +0300

    small improvement

--- src/generator.py
+++ src/generator.py
@@ -100,101 +100,101 @@ def generate_book(conference_obj, list_abstracts, doctpl_filename, finaldocument
             new_paragraph.add_run(section)
             document.add_page_break()
 
-            # for abstract in abstracts_by_tracks[section]:
-            section_abstracts = sorted(abstracts_by_tracks[section], key=by_familyname_key2)
-            for abstract in section_abstracts:
+        # for abstract in abstracts_by_tracks[section]:
+        section_abstracts = sorted(abstracts_by_tracks[section], key=by_familyname_key2)
+        for abstract in section_abstracts:
 
-                all_affiliations = []
-                all_authors = abstract.authors
-                all_authors = sorted(all_authors, key=by_familyname_key)
-                for person in all_authors:
-                    all_affiliations.append(person.affiliation)
+            all_affiliations = []
+            all_authors = abstract.authors
+            all_authors = sorted(all_authors, key=by_familyname_key)
+            for person in all_authors:
+                all_affiliations.append(person.affiliation)
 
-                #all_affiliations_nonrepeat = list(set(all_affiliations))
-                all_affiliations_nonrepeat = []
-                for item in all_affiliations:
-                    if item not in all_affiliations_nonrepeat:
-                        all_affiliations_nonrepeat.append(item)
+            #all_affiliations_nonrepeat = list(set(all_affiliations))
+            all_affiliations_nonrepeat = []
+            for item in all_affiliations:
+                if item not in all_affiliations_nonrepeat:
+                    all_affiliations_nonrepeat.append(item)
 
-                # Writes an abstract.Title in the final document
-                new_paragraph = document.add_paragraph(style=styles["GRID_Title"])
-                new_paragraph.add_run(abstract.title.upper().strip())
+            # Writes an abstract.Title in the final document
+            new_paragraph = document.add_paragraph(style=styles["GRID_Title"])
+            new_paragraph.add_run(abstract.title.upper().strip())
 
-                # Writes abstract.Authors in the final document
-                new_paragraph = document.add_paragraph(style=styles["GRID_author"])
-                aff_index = -1  # affiliation автора
-                email_index = 0  # индекс буквы для email
-                all_authors = abstract.authors
-                all_authors = sorted(all_authors, key=by_familyname_key)
-                # Generates names with indexes
-                for i in range(0, len(all_authors)):
-                    if i > 0:
-                        new_paragraph.add_run(", ")
-                    # FirstName + FamilyName:
-                    new_paragraph.add_run(all_authors[i].first_name.capitalize() + "\xa0" +
-                                          all_authors[i].family_name.capitalize())
-                    # print affiliation index:
-                    if len(all_affiliations_nonrepeat) > 1:
-                        for index, elem in enumerate(all_affiliations_nonrepeat):
-                            if all_authors[i].affiliation == elem:
-                                aff_index = index + 1
-                                break
-                        new_paragraph.add_run(str(aff_index)).font.superscript = True
-                    # print email index only for Primary Author:
-                    if len(all_authors) > 1:
-                        if all_authors[i].is_primary_author:
-                            if all_authors[i].email != "":
-                                if len(all_affiliations_nonrepeat) > 1:
-                                    new_paragraph.add_run("," +
-                                                          string.ascii_lowercase[email_index]).font.superscript = True
-                                else:
-                                    new_paragraph.add_run(string.ascii_lowercase[email_index]).font.superscript = True
-                                email_index += 1
-
-                # Writes abstract.Affiliations
-                aff_index = -1
-                affiliation = ""
-                # Affiliations that we have already written (not to be repeated)
-                typed_affiliations = []
-                for person in all_authors:
-                    if (person.affiliation != "") and person.affiliation not in typed_affiliations:
-                        for index, elem in enumerate(all_affiliations_nonrepeat):
-                            if person.affiliation == elem:  # Looking for a matching element in the set
-                                aff_index = index + 1  # Remember the index of the element
-                                affiliation = elem  # Remember the name of the element
-                                break
-                        new_paragraph = document.add_paragraph(style=styles["GRID_affiliation"])
-                        if len(all_affiliations_nonrepeat) > 1:
-                            new_paragraph.add_run(
-                                str(aff_index)).font.superscript = True  # write the index
-                        new_paragraph.add_run(affiliation)  # write the affiliation
-                        # add affiliation to the list of already written on the page:
-                        typed_affiliations.append(affiliation)
-
-                # write the e-mail
-                count_primary_authors = 0
-                new_paragraph = document.add_paragraph(style=styles["GRID_email"])
-                new_paragraph.add_run("E-mail: ")
-                email_index = 0  # letter for the email
-                for i in range(0, len(all_authors)):
+            # Writes abstract.Authors in the final document
+            new_paragraph = document.add_paragraph(style=styles["GRID_author"])
+            aff_index = -1  # affiliation автора
+            email_index = 0  # индекс буквы для email
+            all_authors = abstract.authors
+            all_authors = sorted(all_authors, key=by_familyname_key)
+            # Generates names with indexes
+            for i in range(0, len(all_authors)):
+                if i > 0:
+                    new_paragraph.add_run(", ")
+                # FirstName + FamilyName:
+                new_paragraph.add_run(all_authors[i].first_name.capitalize() + "\xa0" +
+                                      all_authors[i].family_name.capitalize())
+                # print affiliation index:
+                if len(all_affiliations_nonrepeat) > 1:
+                    for index, elem in enumerate(all_affiliations_nonrepeat):
+                        if all_authors[i].affiliation == elem:
+                            aff_index = index + 1
+                            break
+                    new_paragraph.add_run(str(aff_index)).font.superscript = True
+                # print email index only for Primary Author:
+                if len(all_authors) > 1:
                     if all_authors[i].is_primary_author:
                         if all_authors[i].email != "":
-                            if count_primary_authors > 0:
-                                if i > 0:
-                                    new_paragraph.add_run(", ")
-                            if len(all_authors) > 1:
+                            if len(all_affiliations_nonrepeat) > 1:
+                                new_paragraph.add_run("," +
+                                                      string.ascii_lowercase[email_index]).font.superscript = True
+                            else:
                                 new_paragraph.add_run(string.ascii_lowercase[email_index]).font.superscript = True
                             email_index += 1
-                            new_paragraph.add_run(all_authors[i].email)
-                            count_primary_authors += 1
 
-                #  write the content
-                for x in abstract.content.split(chr(10)):
-                    if x != ' ' and x != '':
-                        new_paragraph = document.add_paragraph(style=styles["GRID_Abstract"])
-                        new_paragraph.add_run(x.replace(chr(9), ''))
+            # Writes abstract.Affiliations
+            aff_index = -1
+            affiliation = ""
+            # Affiliations that we have already written (not to be repeated)
+            typed_affiliations = []
+            for person in all_authors:
+                if (person.affiliation != "") and person.affiliation not in typed_affiliations:
+                    for index, elem in enumerate(all_affiliations_nonrepeat):
+                        if person.affiliation == elem:  # Looking for a matching element in the set
+                            aff_index = index + 1  # Remember the index of the element
+                            affiliation = elem  # Remember the name of the element
+                            break
+                    new_paragraph = document.add_paragraph(style=styles["GRID_affiliation"])
+                    if len(all_affiliations_nonrepeat) > 1:
+                        new_paragraph.add_run(
+                            str(aff_index)).font.superscript = True  # write the index
+                    new_paragraph.add_run(affiliation)  # write the affiliation
+                    # add affiliation to the list of already written on the page:
+                    typed_affiliations.append(affiliation)
 
-                document.add_page_break()
+            # write the e-mail
+            count_primary_authors = 0
+            new_paragraph = document.add_paragraph(style=styles["GRID_email"])
+            new_paragraph.add_run("E-mail: ")
+            email_index = 0  # letter for the email
+            for i in range(0, len(all_authors)):
+                if all_authors[i].is_primary_author:
+                    if all_authors[i].email != "":
+                        if count_primary_authors > 0:
+                            if i > 0:
+                                new_paragraph.add_run(", ")
+                        if len(all_authors) > 1:
+                            new_paragraph.add_run(string.ascii_lowercase[email_index]).font.superscript = True
+                        email_index += 1
+                        new_paragraph.add_run(all_authors[i].email)
+                        count_primary_authors += 1
+
+            #  write the content
+            for x in abstract.content.split(chr(10)):
+                if x != ' ' and x != '':
+                    new_paragraph = document.add_paragraph(style=styles["GRID_Abstract"])
+                    new_paragraph.add_run(x.replace(chr(9), ''))
+
+            document.add_page_break()
 
     document.save(finaldocument_filename)
     print("=======================================================")
